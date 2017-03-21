6f6b3aaa127f754e9a7f2b5ff9211dfe01d231ad
Lawrence Field <lfield@bu.edu>
2017-03-21 15:05:52 -0400

    Refactored code

--- palindrome.rb
+++ palindrome.rb
@@ -2,22 +2,19 @@ class Palindrome
   palindrome_number = 10000
   max_number = 999*999
   palindrome_array = []
-  divisor_array = []
+  
   while palindrome_number < max_number
-    # put all palindromes into an array
-    palindrome_array.push(palindrome_number) if palindrome_number.to_s.reverse.to_i == palindrome_number
+    # determines if a number is a palindrome
+    if palindrome_number.to_s.reverse.to_i == palindrome_number
+      i = 100
+      # determine if a palindrome's number is has two products that are 3 digits
+      while i < 999
+      palindrome_array.push (palindrome_number) if palindrome_number % i == 0 && i <= 999 && palindrome_number / i <= 999
+        i += 1
+      end
+    end
     palindrome_number += 1
   end
-
-  palindrome_array.each do |current_number|
-    i=100
-  while i < 999
-   divisor_array.push(current_number) if current_number % i == 0 && i <= 999 && current_number / i <= 999
-    i += 1
-  end
-  end
-
-print divisor_array.last
+print palindrome_array.last
 end
-
-
+# Created by Lawrence Field on 3/20/2017
